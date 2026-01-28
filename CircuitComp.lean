1a5dceb7a2dc6a7a877bdc530938af06c0e09ea5
Alex Meiburg <timeroot.alex@gmail.com>
2026-01-28 14:18:27 -0500

    update top-level imports

--- CircuitComp.lean
+++ CircuitComp.lean
@@ -1,4 +1,12 @@
 -- This module serves as the root of the `CircuitComp` library.
--- Import modules here that should be built as part of the library.
-import «CircuitComp».NC
-import «CircuitComp».AC
+-- Files are generally sorted by order in the import hierarchy.
+import CircuitComp.ForMathlib
+import CircuitComp.GrowthRate
+import CircuitComp.FuncFamily
+import CircuitComp.Circuit
+import CircuitComp.NC
+import CircuitComp.AC
+import CircuitComp.BranchingProgram.Basic
+import CircuitComp.BranchingProgram.Barrington
+import CircuitComp.BranchingProgram.CanonicalBP
+import CircuitComp.BranchingProgram.ThinBP
